0c9763a1798794bb8b0cca9fe095afe4cbe6b3b8
Josip Mrden <josip.mrden@memgraph.io>
2025-04-21 14:36:08 +0200

    Add device cpu

--- embeddings.py
+++ embeddings.py
@@ -5,7 +5,7 @@ model_name = "all-mpnet-base-v2"
 
 class EmbeddingGenerator:
     def __init__(self):
-        self._model = SentenceTransformer(model_name)
+        self._model = SentenceTransformer(model_name, device="cpu")
     
     def get_embeddings(self, paragraphs: List[str]):
         embeddings = self._model.encode(paragraphs, convert_to_numpy=True)
